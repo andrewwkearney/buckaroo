89f7294bfdbb38d5d2b8e3039faae06173f49d05
Andrew Kearney <andrewwkearney@gmail.com>
2017-09-22 17:23:20 +0100

    Added an inRange and notNegative method with corresponding tests

--- src/main/java/com/androsaces/tools/buckaroo/Params.java
+++ src/main/java/com/androsaces/tools/buckaroo/Params.java
@@ -25,4 +25,84 @@ public class Params {
         if (param == null) throw new IllegalArgumentException(paramName + " cannot be null");
         return param;
     }
+
+    /**
+     * Checks to ensure that the given parameter is not null.
+     *
+     * @param param the parameter being checked.
+     * @return the parameter value.
+     * @throws IllegalArgumentException if the given parameter is null.
+     */
+    public static <T> T notNull(T param) {
+        if (param == null) throw new IllegalArgumentException("param cannot be null");
+        return param;
+    }
+
+    /**
+     * Checks to ensure that the string parameter is not an empty string.
+     *
+     * @param param the string being checked.
+     * @return the parameter value.
+     * @throws IllegalArgumentException if the given parameter is null or an empty string.
+     */
+    public static String notEmpty(String param) {
+        if (notNull(param).isEmpty()) throw new IllegalArgumentException("method parameter cannot be an empty string");
+        return param;
+    }
+
+    /**
+     * Checks to ensure the value passed in is within the given minimum and
+     * maximum values.
+     *
+     * @param p   the number to be checked.
+     * @param min the minimum bound.
+     * @param max the maximum bound.
+     * @return the parameter value.
+     * @throws IllegalArgumentException if the given parameter is not with the range.
+     */
+    public static int inRange(int p, int min, int max) {
+        if (p < min || p > max)
+            throw new IllegalArgumentException("parameter " + p + " not in the range [" + min + ", " + max + ']');
+        return p;
+    }
+
+    /**
+     * Checks to ensure the value passed in is within the given minimum and
+     * maximum values.
+     *
+     * @param p   the number to be checked.
+     * @param min the minimum bound.
+     * @param max the maximum bound.
+     * @return the parameter value.
+     * @throws IllegalArgumentException if the given parameter is not with the range.
+     */
+    public static long inRange(long p, long min, long max) {
+        if (p < min || p > max)
+            throw new IllegalArgumentException("parameter " + p + " not in the range [" + min + ", " + max + ']');
+        return p;
+    }
+
+    /**
+     * Checks to ensure the value passed in is not less than zero.
+     *
+     * @param p the number to be checked.
+     * @return the parameter value.
+     * @throws IllegalArgumentException if the given parameter is less than zero.
+     */
+    public static int notNegative(int p) {
+        if (p < 0) throw new IllegalArgumentException("parameter " + p + " cannot be less than zero");
+        return p;
+    }
+
+    /**
+     * Checks to ensure the value passed in is not less than zero.
+     *
+     * @param p the number to be checked.
+     * @return the parameter value.
+     * @throws IllegalArgumentException if the given parameter is less than zero.
+     */
+    public static long notNegative(long p) {
+        if (p < 0) throw new IllegalArgumentException("parameter " + p + " cannot be less than zero");
+        return p;
+    }
 }
--- src/test/java/com/androsaces/tools/buckaroo/ParamsTest.java
+++ src/test/java/com/androsaces/tools/buckaroo/ParamsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SuppressWarnings("ResultOfMethodCallIgnored")
 class ParamsTest {
     @Test
     @Description("notNull() provided null object")
@@ -18,5 +19,39 @@ class ParamsTest {
     void testNotNullWithNonNullObject() {
         String expected = "expected";
         assertSame(expected, Params.notNull(expected, "expected"));
+    }
+
+    @Test
+    @Description("inRange() provided invalid integer values")
+    void testIntInRangeMethod() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.inRange(0, 1, 2));
+        assertEquals("parameter 0 not in the range [1, 2]", exception.getMessage());
+
+        exception = assertThrows(IllegalArgumentException.class, () -> Params.inRange(-1, -2, -4));
+        assertEquals("parameter -1 not in the range [-2, -4]", exception.getMessage());
+    }
+
+    @Test
+    @Description("inRange() provided invalid long values")
+    void testLongInRangeMethod() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.inRange(0L, 1L, 2L));
+        assertEquals("parameter 0 not in the range [1, 2]", exception.getMessage());
+
+        exception = assertThrows(IllegalArgumentException.class, () -> Params.inRange(-1L, -2L, -4L));
+        assertEquals("parameter -1 not in the range [-2, -4]", exception.getMessage());
+    }
+
+    @Test
+    @Description("notNegative() provided invalid int values")
+    void testIntNotNegative() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNegative(-1));
+        assertEquals("parameter -1 cannot be less than zero", exception.getMessage());
+    }
+
+    @Test
+    @Description("notNegative() provided invalid long values")
+    void testLongNotNegative() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNegative(-1L));
+        assertEquals("parameter -1 cannot be less than zero", exception.getMessage());
     }
 }
