aa6809da1d4e907a4f39e852bac4e2549bb48a4b
Andrew Kearney <andrewwkearney@gmail.com>
2017-10-09 10:52:06 +0100

    Changed unit test to use the new @DisplayName annotation rather than the old @Description

--- src/test/java/com/androsaces/tools/buckaroo/ParamsTest.java
+++ src/test/java/com/androsaces/tools/buckaroo/ParamsTest.java
@@ -1,6 +1,6 @@
 package com.androsaces.tools.buckaroo;
 
-import com.sun.org.glassfish.gmbal.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,21 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @SuppressWarnings("ResultOfMethodCallIgnored")
 class ParamsTest {
     @Test
-    @Description("notNull() provided null object")
+    @DisplayName("notNull() provided null object")
     void testNotNullWithNullObject() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNull(null, "null"));
         assertEquals("null cannot be null", exception.getMessage());
     }
 
     @Test
-    @Description("notNull() provided non-null object")
+    @DisplayName("notNull() provided non-null object")
     void testNotNullWithNonNullObject() {
         String expected = "expected";
         assertSame(expected, Params.notNull(expected, "expected"));
     }
 
     @Test
-    @Description("inRange() provided invalid integer values")
+    @DisplayName("inRange() provided invalid integer values")
     void testIntInRangeMethod() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.inRange(0, 1, 2));
         assertEquals("parameter 0 not in the range [1, 2]", exception.getMessage());
@@ -32,7 +32,7 @@ class ParamsTest {
     }
 
     @Test
-    @Description("inRange() provided invalid long values")
+    @DisplayName("inRange() provided invalid long values")
     void testLongInRangeMethod() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.inRange(0L, 1L, 2L));
         assertEquals("parameter 0 not in the range [1, 2]", exception.getMessage());
@@ -42,14 +42,14 @@ class ParamsTest {
     }
 
     @Test
-    @Description("notNegative() provided invalid int values")
+    @DisplayName("notNegative() provided invalid int values")
     void testIntNotNegative() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNegative(-1));
         assertEquals("parameter -1 cannot be less than zero", exception.getMessage());
     }
 
     @Test
-    @Description("notNegative() provided invalid long values")
+    @DisplayName("notNegative() provided invalid long values")
     void testLongNotNegative() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNegative(-1L));
         assertEquals("parameter -1 cannot be less than zero", exception.getMessage());
