8c5b9e04fe3344997efc0cf155152874bbbc8ec4
Andrew Kearney <andrewwkearney@gmail.com>
2017-10-21 21:30:57 +0100

    Added tests to ensure coverage

--- src/test/java/com/androsaces/tools/buckaroo/ParamsTest.java
+++ src/test/java/com/androsaces/tools/buckaroo/ParamsTest.java
@@ -5,20 +5,51 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SuppressWarnings("ResultOfMethodCallIgnored")
+// @SuppressWarnings("ResultOfMethodCallIgnored")
 class ParamsTest {
     @Test
-    @DisplayName("notNull() provided null object")
-    void testNotNullWithNullObject() {
+    @DisplayName("two parameter notNull() provided null object")
+    void testTwoElementNotNullWithNullObject() {
+        String expected = "null cannot be null";
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNull(null, "null"));
-        assertEquals("null cannot be null", exception.getMessage());
+        assertEquals(expected, exception.getMessage());
     }
 
     @Test
-    @DisplayName("notNull() provided non-null object")
-    void testNotNullWithNonNullObject() {
+    @DisplayName("two parameter notNull() provided non-null object")
+    void testTwoElementNotNullWithNonNullObject() {
         String expected = "expected";
         assertSame(expected, Params.notNull(expected, "expected"));
+    }
+
+    @Test
+    @DisplayName("one parameter notNull() provided null object")
+    void testOneElementNotNullWithNullObject() {
+        String expected = "parameter cannot be null";
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNull(null));
+        assertEquals(expected, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("one parameter notNull() provided non-null object")
+    void testOneElementNotNullWithNonNullObject() {
+        String expected = "expected";
+        assertSame(expected, Params.notNull(expected));
+    }
+
+    @Test
+    @DisplayName("notEmpty() provided with an empty string")
+    void testEmptyStringWithAnEmptyString() {
+        String expected = "";
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notEmpty(expected));
+        assertEquals("method parameter cannot be an empty string", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("notEmpty() provided a non-empty string")
+    void testEmptyStringWithNonEmptyString() {
+        String expected = "Test String";
+        assertSame(expected, Params.notEmpty(expected));
     }
 
     @Test
@@ -32,6 +63,13 @@ class ParamsTest {
     }
 
     @Test
+    @DisplayName("inRange() provided valid integer value")
+    void testIntInRangeWithValidValue() {
+        int expected = 10;
+        assertSame(expected, Params.inRange(expected, 0, Integer.MAX_VALUE));
+    }
+
+    @Test
     @DisplayName("inRange() provided invalid long values")
     void testLongInRangeMethod() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.inRange(0L, 1L, 2L));
@@ -42,6 +80,13 @@ class ParamsTest {
     }
 
     @Test
+    @DisplayName("inRange() provided valid integer value")
+    void testLongInRangeWithValidValue() {
+        long expected = 10L;
+        assertSame(expected, Params.inRange(expected, 0L, Long.MAX_VALUE));
+    }
+
+    @Test
     @DisplayName("notNegative() provided invalid int values")
     void testIntNotNegative() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNegative(-1));
@@ -49,9 +94,23 @@ class ParamsTest {
     }
 
     @Test
+    @DisplayName("notNegative() provided positive value")
+    void testIntNotNegativeWithPositiveValue() {
+        int expected = 10;
+        assertSame(expected, Params.notNegative(expected));
+    }
+
+    @Test
     @DisplayName("notNegative() provided invalid long values")
     void testLongNotNegative() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Params.notNegative(-1L));
         assertEquals("parameter -1 cannot be less than zero", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("notNegative() provided positive value")
+    void testLongNotNegativeWithPositiveValue() {
+        long expected = 10L;
+        assertSame(expected, Params.notNegative(expected));
     }
 }
