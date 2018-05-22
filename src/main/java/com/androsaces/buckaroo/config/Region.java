950761b6d8eb5e82d031affc208252e048bb337e
Andrew Kearney <andrewwkearney@gmail.com>
2018-05-22 10:13:09 +0100

    Added javadocs to the regions

--- src/main/java/com/androsaces/buckaroo/config/Region.java
+++ src/main/java/com/androsaces/buckaroo/config/Region.java
@@ -11,9 +11,32 @@ package com.androsaces.buckaroo.config;
  * @since 1.1.0
  */
 public enum Region {
+    /**
+     * The global region of the Americas.
+     * <p>Typically includes North, Central and South America.
+     */
     AMER("Americas"),
+    /**
+     * The global region of Asia Pacific.
+     * <p>
+     * Typically includes countries on the Pacific rim, like China, Japan,
+     * India, Sri Lanka, Singapore, Australia and New Zealand. May also
+     * include far east Russia.
+     */
     APAC("Asia Pacific"),
+    /**
+     * The global region of Europe, Middle East and Africa.
+     * <p>
+     * Typically includes continental Europe, Africa and the Middle East and
+     * sometimes may include western Russia.
+     * </p>
+     */
     EMEA("Europe, Middle East and Africa"),
+    /**
+     * Global region.
+     * <p>
+     * Encompasses all regions.
+     */
     GLOB("Global");
 
     private final String description;
