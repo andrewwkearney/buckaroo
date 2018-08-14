dea3dab7cc790ae12262d46be6c0eed92a1c3391
Andrew Kearney <andrewwkearney@gmail.com>
2018-08-14 14:22:32 +0100

    Added a nullable annotation and refactored notnull to match the standard

--- src/main/java/com/androsaces/buckaroo/annotation/Nullable.java
+++ src/main/java/com/androsaces/buckaroo/annotation/Nullable.java
@@ -7,13 +7,13 @@ package com.androsaces.buckaroo.annotation;
 import java.lang.annotation.*;
 
 /**
- * Any element that has been annotated with {@code NotNull} claims that {@code null}
- * is a forbidden value.
+ * Any element that has been annotated with {@code Nullable} claims that {@code null}
+ * is an acceptable value.
  *
  * @author Andrew Kearney
  */
 @Documented
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
-public @interface NotNull {
+public @interface Nullable {
 }
