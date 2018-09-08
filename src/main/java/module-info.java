f03155e36cad0689bb32c41887093cf5e13d1e66
Andrew Kearney <andrewwkearney@gmail.com>
2018-09-08 13:18:07 +0100

    Added module-info to assist with usage as a library and updated dependencies to compile with Java 10

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module com.androsaces.buckaroo {
+    exports com.androsaces.buckaroo;
+    exports com.androsaces.buckaroo.annotation;
+    exports com.androsaces.buckaroo.collection;
+    exports com.androsaces.buckaroo.config;
+    exports com.androsaces.buckaroo.pattern;
+
+    requires java.base;
+    requires org.slf4j;
+}
