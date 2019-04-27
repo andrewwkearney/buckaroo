cf6d262c4d59ab6d491f25a991abe6a145394c4d
Andrew Kearney <andrewwkearney@gmail.com>
2019-04-27 18:31:16 +0100

    Created an empty arrays class

---
+++ src/main/java/com/androsaces/buckaroo/EmptyArrays.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2019 androsaces. All rights reserved.
+ */
+
+package com.androsaces.buckaroo;
+
+/**
+ * This class provides a set of constant empty arrays.
+ *
+ * @author Andrew Kearney
+ * @since 2.1.0
+ */
+public class EmptyArrays {
+    public static final Class[] EMPTY_CLASS_ARRAY = new Class[0];
+    public static final Object[] EMPTY_OBJECT_ARRAY = new Object[0];
+    public static final String[] EMPTY_STRING_ARRAY = new String[0];
+
+    public static final boolean[] EMPTY_BOOLEAN_ARRAY = new boolean[0];
+    public static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+    public static final char[] EMPTY_CHAR_ARRAY = new char[0];
+    public static final double[] EMPTY_DOUBLE_ARRAY = new double[0];
+    public static final float[] EMPTY_FLOAT_ARRAY = new float[0];
+    public static final int[] EMPTY_INT_ARRAY = new int[0];
+    public static final long[] EMPTY_LONG_ARRAY = new long[0];
+    public static final short[] EMPTY_SHORT_ARRAY = new short[0];
+
+    public static final Boolean[] EMPTY_BOOLEAN_W_ARRAY = new Boolean[0];
+    public static final Byte[] EMPTY_BYTE_W_ARRAY = new Byte[0];
+    public static final Character[] EMPTY_CHAR_W_ARRAY = new Character[0];
+    public static final Double[] EMPTY_DOUBLE_W_ARRAY = new Double[0];
+    public static final Float[] EMPTY_FLOAT_W_ARRAY = new Float[0];
+    public static final Integer[] EMPTY_INT_W_ARRAY = new Integer[0];
+    public static final Long[] EMPTY_LONG_W_ARRAY = new Long[0];
+    public static final Short[] EMPTY_SHORT_W_ARRAY = new Short[0];
+
+    private EmptyArrays() {
+    }
+}
